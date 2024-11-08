c98f1a21763a58325a8e4291295622dcc6991b9a
Youngrae Cho <yrc9229@gmail.com>
2024-11-08 10:46:23 +0900

    와일드카드 클래스명 수정

--- src/main/java/book/jmb/chapter08/wildcard/Final.java
+++ src/main/java/book/jmb/chapter08/wildcard/Final.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Main {
+public class Final {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
