a955b0703ea9baf925500ec79d0d68a2d1814051
Youngrae Cho <yrc9229@gmail.com>
2024-11-11 09:56:10 +0900

    삼각형 위의 최대 경로 코드 정리

--- src/main/java/book/jmb/chapter08/trianglepath/Code_8_9.java
+++ src/main/java/book/jmb/chapter08/trianglepath/Code_8_9.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Main {
+public class Code_8_9 {
 
     private static int n;
     private static int[][] triangle;
