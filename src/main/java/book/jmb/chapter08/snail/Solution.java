b47704ca01fd36775ab4ff47143ac3829372b37e
Youngrae Cho <yrc9229@gmail.com>
2024-11-13 10:15:45 +0900

    달팽이 정답 클래스 명 수정

--- src/main/java/book/jmb/chapter08/snail/Solution.java
+++ src/main/java/book/jmb/chapter08/snail/Solution.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Solution {
 
     private static int n;
     private static int m;
